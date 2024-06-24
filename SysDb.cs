69f4134878c0797a8242da9dbcf56aa40d2fbb4b
ciao0108 <155428799+ciao0108@users.noreply.github.com>
2024-06-24 15:24:14 +0800

    Add files via upload

---
+++ SysDb.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/// <summary>
+/// SysDb 的摘要描述
+/// </summary>
+public class SysDb : SqlDataSource
+{
+    public SysDb(): base(System.Configuration.ConfigurationManager.ConnectionStrings["MIS_3"].ConnectionString, "")
+    {
+    }
+    public SysDb(String constr) 
+        : base(System.Configuration.ConfigurationManager.ConnectionStrings[constr].ConnectionString, "")
+    {
+    }
+
+
+}
